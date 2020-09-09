8658887cf818721a13944445d2c31e19a8757e7e
Parsa Torbati <ptorbatii@gmail.com>
2020-09-09 20:59:42 +0430

    made the shortlink not include the characters l, I and 1 to avoid confusion

--- xyloid/utils.py
+++ xyloid/utils.py
@@ -1,7 +1,5 @@
 import random
 
 def create_shortlink():
-    chars = "abcdefghijklmnopqrstuvwxyz"
-    chars += chars.upper()
-    chars += "1234567890"
+	chars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890"
     return "".join([random.choice(chars) for i in range(16)])
