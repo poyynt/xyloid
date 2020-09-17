adc744e3122597b0833425b5d8867fcd3737ae75
Parsa Torbati <ptorbatii@gmail.com>
2020-09-17 11:13:24 +0430

    change shortlink length to 8

--- xyloid/utils.py
+++ xyloid/utils.py
@@ -2,4 +2,4 @@ import random
 
 def create_shortlink():
 	chars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890"
-	return "".join([random.choice(chars) for i in range(16)])
+	return "".join([random.choice(chars) for i in range(8)])
