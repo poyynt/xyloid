c964501d224db9b0a67ff9b61d1710c369ef5522
Parsa Torbati <ptorbatii@gmail.com>
2020-09-19 09:03:04 +0430

    add a new /count/category endpoint for counting posts in a category

--- xyloid/api/api.py
+++ xyloid/api/api.py
@@ -31,7 +31,7 @@ def all_posts(page = 1):
 	result = [{"name": r.name, "uuid": r.uuid, "shortlink": r.shortlink, "created": r.created, "categories": [r.category1, r.category2, r.category3, r.category4, r.category5]} for r in result]
 	return jsonify(result)
 
-@api.route("/posts/count")
+@api.route("/posts/count/all")
 def posts_count():
 	return str(Posts.query.count())
 
@@ -120,6 +120,13 @@ def posts_by_category(category, page = 1):
 		result = list(Posts.query.filter_by(category1="", category2="", category3="", category4="", category5="").order_by(desc(Posts.internal_id)).paginate(page=page, per_page=20, max_per_page=20).items)
 	result = [{"uuid": r.uuid} for r in result]
 	return jsonify(result)
+@api.route("/posts/count/<category>")
+def category_count(category):
+	if category not in ("uncategorized"):
+		result = Posts.query.filter((Posts.category1 == category) | (Posts.category2 == category) | (Posts.category3 == category) | (Posts.category4 == category) | (Posts.category5 == category)).count()
+	else:
+		result = Posts.query.filter_by(category1="", category2="", category3="", category4="", category5="").count()
+	return str(result)
 
 @api.route("/categories")
 def get_categories():
